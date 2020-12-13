caecd41bb6fa997eb7bc5f566032facff353e483
subban358 <subhambanerjee138@gmail.com>
2020-12-13 17:58:05 +0530

    RestTemplate Bean Created and Service added

--- src/main/java/com/student/api/student_api/StudentApiApplication.java
+++ src/main/java/com/student/api/student_api/StudentApiApplication.java
@@ -1,13 +1,25 @@
 package com.student.api.student_api;
 
-import com.student.api.student_dto.StudentDTO;
+import com.student.api.student_api.service.StudentAPIService;
+import com.student.api.student_api.student_dto.StudentDTO;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class StudentApiApplication implements CommandLineRunner {
+
+	@Autowired
+	private StudentAPIService serstudentAPIServicevice;
+
+
+	@Bean
+	public RestTemplate getRestTemplate(){
+		return new RestTemplate();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(StudentApiApplication.class, args);
@@ -15,27 +27,15 @@ public class StudentApiApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		getStudentDetails(2);
+		serstudentAPIServicevice.getStudentDetails(1);
 
 		StudentDTO student = new StudentDTO();
 		student.setName("Ritovash Mazumder");
 		student.setDept("CSE");
 		student.setAge(21);
 
-		addStudent(student);
+		serstudentAPIServicevice.addStudent(student);
 	}
 
-	public void getStudentDetails(Integer studentId){
-		String url = "http://localhost:8000/api/student/{studentID}";
-		RestTemplate restTemplate = new RestTemplate();
-		StudentDTO studentDTO = restTemplate.getForObject(url, StudentDTO.class, studentId);
-		System.out.println(studentDTO);
-	}
 
-	public void addStudent(StudentDTO studentDTO){
-		String url = "http://localhost:8000/api/student/";
-		RestTemplate restTemplate = new RestTemplate();
-		StudentDTO addedStudent = restTemplate.postForObject(url, studentDTO, StudentDTO.class);
-		System.out.println(addedStudent);
-	}
 }
